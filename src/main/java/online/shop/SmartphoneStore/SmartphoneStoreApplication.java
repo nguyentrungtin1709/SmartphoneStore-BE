3e2fe4425a45166b76f17ae546181a5c82eeba68
Lyridsss <trungtincom7@gmail.com>
2023-08-28 22:37:04 +0700

    Khởi tạo dự án

---
+++ src/main/java/online/shop/SmartphoneStore/SmartphoneStoreApplication.java
@@ -0,0 +1,13 @@
+package online.shop.SmartphoneStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartphoneStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartphoneStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/shop/SmartphoneStore/SmartphoneStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package online.shop.SmartphoneStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartphoneStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
