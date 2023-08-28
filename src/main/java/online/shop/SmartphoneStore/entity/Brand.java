b863ff650dc7e8eea884cdbf31bc1b1fd52c695c
Lyridsss <trungtincom7@gmail.com>
2023-08-29 01:15:30 +0700

    Thêm Hãng sản xuất

--- src/main/java/online/shop/SmartphoneStore/entity/Brand.java
+++ src/main/java/online/shop/SmartphoneStore/entity/Brand.java
@@ -1,7 +1,30 @@
 package online.shop.SmartphoneStore.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-//@Entity
-//public class Brand {
-//}
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(
+        name = "hang_san_xuat"
+)
+public class Brand {
+    @Id
+    @GeneratedValue
+    @Column(name = "ma_hang")
+    private Integer id;
+
+    @NotNull
+    @NotBlank(message = "Không được bỏ trống")
+    @Column(name = "ten_hang")
+    private String name;
+
+}
