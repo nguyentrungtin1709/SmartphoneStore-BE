3a6ebba11c0bffd710b20c9ce3b23cadcc5f498d
Lyridsss <trungtincom7@gmail.com>
2023-08-29 01:22:03 +0700

    Thêm Nhà cung cấp

--- src/main/java/online/shop/SmartphoneStore/entity/Supplier.java
+++ src/main/java/online/shop/SmartphoneStore/entity/Supplier.java
@@ -1,7 +1,29 @@
 package online.shop.SmartphoneStore.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-//@Entity
-//public class Supplier {
-//}
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(
+        name = "nha_cung_cap"
+)
+public class Supplier {
+    @Id
+    @GeneratedValue
+    @Column(name = "ma_ncc")
+    private Integer id;
+
+    @NotNull
+    @NotBlank(message = "Không được bỏ trống")
+    @Column(name = "ten_ncc")
+    private String name;
+}
