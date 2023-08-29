d4a417c4340a7fd8c3bcf8179ee8abda04af3e0d
Lyridsss <trungtincom7@gmail.com>
2023-08-30 01:25:35 +0700

    Thêm chi tiết nhập hàng và thêm kiểm tra những ràng buộc

--- src/main/java/online/shop/SmartphoneStore/entity/ImportDetails.java
+++ src/main/java/online/shop/SmartphoneStore/entity/ImportDetails.java
@@ -1,8 +1,25 @@
 package online.shop.SmartphoneStore.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
 
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(
+        name = "chi_tiet_nhap_hang",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "unique_mnh_sku",
+                        columnNames = {"ma_nhap_hang", "sku"}
+                )
+        }
+)
 public class ImportDetails {
 
     @Id
@@ -11,10 +28,27 @@ public class ImportDetails {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "ma_nhap_hang", nullable = false)
+    @JoinColumn(
+            name = "ma_nhap_hang",
+            nullable = false
+    )
     private Import anImport;
 
+    @ManyToOne
+    @JoinColumn(
+            name = "sku",
+            nullable = false,
+            referencedColumnName = "sku"
+    )
+    private Smartphone smartphone;
 
+    @NotNull
+    @Positive
+    @Column(name = "so_luong")
+    private Integer quantity;
 
-
+    @NotNull
+    @Positive
+    @Column(name = "gia_nhap")
+    private Double price;
 }
