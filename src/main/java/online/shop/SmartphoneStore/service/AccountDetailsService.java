30debc4b2bc52fe0af20e0783fe4287be92ed2d3
Lyridsss <trungtincom7@gmail.com>
2023-09-03 19:32:38 +0700

    Thêm AccountDetailsService

---
+++ src/main/java/online/shop/SmartphoneStore/service/AccountDetailsService.java
@@ -0,0 +1,26 @@
+package online.shop.SmartphoneStore.service;
+
+import online.shop.SmartphoneStore.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountDetailsService implements UserDetailsService {
+
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountDetailsService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return accountRepository
+                .findAccountByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Tài khoản không tồn tại"));
+    }
+}
